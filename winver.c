519ff927de168c5604bfbe5b09fb37de67716e25
prokushev <yuri.prokushev@gmail.com>
2026-05-23 12:16:04 +0700

    Now works under Win 3.0 (except Real Mode kernel)

--- winver.c
+++ winver.c
@@ -24,26 +24,107 @@
 
 typedef BOOL (WINAPI *SHELLABOUT)(HWND hWnd, LPCSTR lpszCaption, LPCSTR lpszAboutText, HICON hIcon);
 
-int PASCAL WinMain(HINSTANCE inst, HINSTANCE prev, LPSTR cmdline, int show)
+char szVersionBuf[128];
+
+BOOL WINAPI VersionDlgProc(HWND hDlg, UINT msg, WPARAM wParam, LPARAM lParam)
 {
-	char szTitle[50];
-	SHELLABOUT lpShellAbout;
-	int retVal=0;
-
-	LoadString(inst, IDS_PACKAGE_NAME, szTitle, sizeof(szTitle));
-
-	//Needed for Win95 and later??
-	//InitCommonControls();
-
-	lpShellAbout=(SHELLABOUT)GetProcAddress(GetModuleHandle("shell.dll"), "ShellAbout");
-
-	if (lpShellAbout)
-	{
-		retVal=!lpShellAbout(0, szTitle, NULL, NULL);
-	} else {
-		// Most probably, we are in Windows 3.0 or earler, so no ShellAbout
-		// Use own dialog, not standard...
-	}
-	return retVal;
+    switch (msg)
+    {
+    case WM_INITDIALOG:
+        SetDlgItemText(hDlg, IDC_VERSION_TEXT, szVersionBuf);
+        return TRUE;
+    case WM_COMMAND:
+        EndDialog(hDlg, 1);
+        return TRUE;
+    }
+    return FALSE;
 }
 
+int PASCAL WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR cmdline, int show)
+{
+    char szTitle[50];
+    DWORD dwVersion, dwFlags;
+    WORD  wVersion;
+    int   nMajor, nMinor, nDosMajor, nDosMinor;
+    LPSTR lpMode  = "";
+    LPSTR lpDebug = "";
+    char  szFpu[16] = "";
+    char  szDosVer[32] = "";
+    FARPROC lpfnDlgProc;
+    HINSTANCE hShell;
+    SHELLABOUT pShellAbout;
+    WORD oldErrorMode;
+  
+    if (hPrevInstance) return 0;
+
+    LoadString(hInstance, IDS_PACKAGE_NAME, szTitle, sizeof(szTitle));
+
+    /* Try shell.dll first (@todo Does'n work under Real Mode Windows 3.0) */
+    oldErrorMode = SetErrorMode(SEM_NOOPENFILEERRORBOX);
+    hShell = LoadLibrary("SHELL.DLL");
+    SetErrorMode(oldErrorMode);
+    if (hShell)
+    {
+        pShellAbout = (SHELLABOUT)GetProcAddress(hShell, "ShellAbout");
+        if (pShellAbout)
+        {
+            BOOL bRet = pShellAbout(NULL, szTitle, NULL, NULL);
+            FreeLibrary(hShell);
+            return !bRet;
+        }
+        FreeLibrary(hShell);
+    }
+
+    /* Get version */
+    dwVersion = GetVersion();
+    wVersion  = LOWORD(dwVersion);
+    nMajor    = LOBYTE(wVersion);
+    nMinor    = HIBYTE(wVersion);
+
+    /* Get DOS version from high word  */
+    nDosMajor = HIBYTE(HIWORD(dwVersion));
+    nDosMinor = LOBYTE(HIWORD(dwVersion));
+
+    /* Get winflags */
+    dwFlags = GetWinFlags();
+
+
+    /* set current mode */
+    if (dwFlags & WF_PMODE)
+    {
+        if (dwFlags & WF_STANDARD)
+            lpMode = "Standard Mode";
+        else
+            lpMode = "386 Enhanced Mode";
+    }
+    else
+    {
+        if (dwFlags & WF_LARGEFRAME)
+            lpMode = "Large Frame EMS";
+        else if (dwFlags & WF_SMALLFRAME)
+            lpMode = "Small Frame EMS";
+        else
+            lpMode = "Real Mode";
+    }
+
+    /* Debug version */
+    if (GetSystemMetrics(SM_DEBUG))
+        lpDebug = "DEBUG ";
+
+    /* Check for 80x87 coprocessor */
+    if (dwFlags & WF_80x87)
+        lstrcpy(szFpu, " [80x87]");
+
+    /* Format DOS version string if available */
+    if (nDosMajor > 0 || nDosMinor > 0)
+        wsprintf(szDosVer, " (DOS %d.%02d)", nDosMajor, nDosMinor);
+
+    wsprintf(szVersionBuf, "%s osFree Janus %sVersion %d.%02d%s%s",
+             lpMode, lpDebug, nMajor, nMinor, (LPSTR)szDosVer, (LPSTR)szFpu);
+
+    lpfnDlgProc = MakeProcInstance((FARPROC)VersionDlgProc, hInstance);
+    DialogBox(hInstance, MAKEINTRESOURCE(IDD_VERSION_DLG), NULL, lpfnDlgProc);
+    FreeProcInstance(lpfnDlgProc);
+
+    return 1;
+}
